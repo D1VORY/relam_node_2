cf517157cf3f9d444b7e9099e3c908cbea787ef9
Olexandr <dubchakob@gmail.com>
2022-05-10 22:43:00 +0300

    cleaned up

--- temp.js
+++ temp.js
@@ -1,7 +1,6 @@
 Realm = require('realm');
 var fs = require('fs');
 
-var all_dnks_json = JSON.parse(fs.readFileSync('complete2.json', 'utf8'));
 
 
 const TaskSchema = {
@@ -31,45 +30,24 @@ const DNKSchema = {
   primaryKey: '_id',
 };
 
-//const app = new Realm.App({id: realmAppId})
-// Realm.open({path: "myrealm", schema: [TaskSchema],}).then(
-//     (res) =>{kek = res}
-// );
 
-//const kek = Realm.open({path: "myrealm", schema: [TaskSchema],}).then(res => res);
-
+async function load_from_json(rlm){
+  var all_dnks_json = JSON.parse(fs.readFileSync('complete2.json', 'utf8'));
+   realm.write(() => {
+     all_dnks_json.map((obj, i) => {
+        realm.create(DNKSchema.name, {
+          _id: i,
+          ...obj
+        });
+     })
+   })
+}
 
 const realm_open = async () => {
    let realm = await Realm.open({
     path: "myrealm",
     schema: [TaskSchema, DNKSchema],
   })
-
-
-  // let task1, task2;
-  //  realm.write(() => {
-  //    all_dnks_json.map((obj, i) => {
-  //       realm.create(DNKSchema.name, {
-  //         _id: i,
-  //         ...obj
-  //       });
-  //    })
-  //  })
-  //
-  // realm.write(() => {
-  //   task1 = realm.create("Task", {
-  //     _id: 1,
-  //     name: "go grocery shopping",
-  //     status: "Open",
-  //   });
-  //   task2 = realm.create("Task", {
-  //     _id: 2,
-  //     name: "go exercise",
-  //     status: "Open",
-  //   });
-  //   console.log(`created two tasks: ${task1.name} & ${task2.name}`);
-  // });
-
   const dnks = realm.objects("DNK");
   console.log(`The lists of tasks are: ${dnks.map((task) => (JSON.stringify(task)))}`);
 }
